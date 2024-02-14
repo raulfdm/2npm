870fe09349d1d2b326e06692f7700ae154c93a4d
Raul Melo <melo.raulf@gmail.com>
2024-02-14 08:21:30 +0100

    move utilities to utils package

--- packages/utils/vite.config.ts
+++ packages/utils/vite.config.ts
@@ -5,6 +5,8 @@ import dts from "vite-plugin-dts";
 export default defineConfig({
 	plugins: [dts()],
 	build: {
+		ssr: true,
+		target: "node18",
 		lib: {
 			entry: "src/index.ts",
 			formats: ["es"],
